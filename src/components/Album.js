5c90146e15ba6753d7e6fe650643e486d4459987
Lucas Israel <lisraelalves92@gmail.com>
2022-08-02 14:01:35 -0300

    7 feito

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from './MusicCard';
 
 class Album extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      album: [],
+      musicas: [],
+    };
+  }
+
+  async componentDidMount() {
     const { match: { params: { id } } } = this.props;
+    const value = await getMusics(id);
+    this.setState({
+      album: value.filter((_element, index) => index === 0),
+      musicas: value.filter((_element, index) => index !== 0),
+    });
+  }
+
+  render() {
+    const { album, musicas } = this.state;
+    const albumName = album.map(({ collectionCensoredName }) => (
+      <p key={ collectionCensoredName } data-testid="album-name">
+        Collection Name
+        {': '}
+        {collectionCensoredName}
+      </p>
+    ));
+    const artistNam = album.map(({ artistName }) => (
+      <p key={ artistName } data-testid="artist-name">
+        {`Artista: ${artistName}`}
+      </p>));
+    const albumPicture = album.map(({ artistId, artworkUrl100 }) => (
+      <div key={ artistId }>
+        <img src={ artworkUrl100 } alt={ artistId } />
+      </div>
+    ));
     return (
-      <div data-testid="page-album" className="page">
+      <div data-testid="page-album">
         <Header />
-        Album
-        {id}
+        <div className="page">
+          {albumPicture}
+          {artistNam}
+          {albumName}
+          {musicas.map(({ previewUrl, trackId, trackName }) => (
+            <div key={ trackId } className="tracks">
+              <MusicCard previewUrl={ previewUrl } trackName={ trackName } />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
---
+++ src/components/MusicCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <div className="track">
+        <div>
+          {trackName}
+        </div>
+        <div>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+        </div>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
